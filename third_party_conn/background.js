62569356fba0f2e35a61be0eaa5816676d18cf34
P-ASilva <pedroas5.al.insper.edu>
2024-10-21 13:11:59 -0300

    basic reset logic

--- third_party_conn/background.js
+++ third_party_conn/background.js
@@ -1,25 +1,29 @@
 browser.webRequest.onBeforeRequest.addListener(
   function(details) {
-    console.log(`Conexão de terceira parte detectada: ${details.originUrl} -> ${details.url}`);
-    let url = new URL(details.url);         // Domínio da requisição
-    let requestedDomain = url.hostname;     // Domínio do recurso solicitado
+    let url = new URL(details.url);
+    let requestedDomain = url.hostname;
     let originDomain = null;
 
+
+    // Armazena o domínio de origem no localStorage e reseta as conexões se mudar
+    let storedOriginDomain = localStorage.getItem('originDomain');
+    if (storedOriginDomain !== originDomain) {
+        localStorage.setItem('originDomain', originDomain);
+        localStorage.setItem('thirdPartyConnections', JSON.stringify([]));
+    }
     // Tenta pegar o domínio da página usando 'originUrl' ou 'tabId'
     if (details.originUrl) {
       let originUrl = new URL(details.originUrl);
-      originDomain = originUrl.hostname;    // Dominio da página de origem
+      originDomain = originUrl.hostname;
     } else if (details.tabId !== -1) {
       browser.tabs.get(details.tabId).then(tab => {
         let tabUrl = new URL(tab.url);
-        originDomain = tabUrl.hostname;     // Domínio da aba ativa
+        originDomain = tabUrl.hostname;
       });
     }
 
     // Verifica se o domínio da requisição é diferente do domínio da página
     if (originDomain !== requestedDomain) {
-      console.log(`Conexão de terceira parte detectada: ${requestedDomain}`);
-
       // Armazena o domínio detectado no localStorage
       let storedConnections = JSON.parse(localStorage.getItem('thirdPartyConnections')) || [];
       if (!storedConnections.includes(requestedDomain)) {
@@ -27,6 +31,8 @@ browser.webRequest.onBeforeRequest.addListener(
         localStorage.setItem('thirdPartyConnections', JSON.stringify(storedConnections));
       }
     }
+
+
   },
   {urls: ["<all_urls>"]}
 );
